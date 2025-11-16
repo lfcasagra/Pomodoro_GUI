89081c513bf515d290b274f1c209314460975a9a
Luiz Casagrande <47531037+lfcasagra@users.noreply.github.com>
2025-11-16 15:37:04 -0300

    VS Code

---
+++ Pomodoro_GUI/# This program adds two numbers.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+# Take input from the stdin
+x=int(input("Please enter an integer: "));
+y=int(input("Please enter an integer: "));
+
+# Add two numbers
+z=x+y;
+
+# Display the sum in the console.
+print ('Sum of x+y =',z)
